e459d7971c084df8845cd7601da215ced20cef07
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-10-28 16:03:30 -0400

    teehee

--- MarrowLauncher/Program.cs
+++ MarrowLauncher/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net;
+using System.Runtime.InteropServices;
 using Octokit;
 
-GitHubClient client = new GitHubClient(new ProductHeaderValue("SomeName"));
+GitHubClient client = new GitHubClient(new ProductHeaderValue("protogenposting"));
 
 IReadOnlyList<Release> releases = await client.Repository.Release.GetAll("protogenposting", "Marrow");
 
@@ -10,4 +12,35 @@ Release latest = releases[0];
 
 var webClient = new WebClient();
 
-webClient.DownloadFile(new Uri(latest.ZipballUrl), "Marrow.zip");
+webClient.UseDefaultCredentials = true;
+
+webClient.DownloadFile("https://github.com/protogenposting/Marrow/releases/latest/download/Marrow.jar", "Marrow.jar");
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+{
+    RunCommandWithBash("java Marrow.jar");
+}
+else
+{
+    string strCmdText;
+    strCmdText= "java Marrow.jar";
+    System.Diagnostics.Process.Start("CMD.exe",strCmdText);
+}
+
+string RunCommandWithBash(string command)
+{
+    var psi = new ProcessStartInfo();
+    psi.FileName = "/bin/bash";
+    psi.Arguments = command;
+    psi.RedirectStandardOutput = true;
+    psi.UseShellExecute = false;
+    psi.CreateNoWindow = true;
+
+    using var process = Process.Start(psi);
+
+    process.WaitForExit();
+
+    var output = process.StandardOutput.ReadToEnd();
+
+    return output;
+}
