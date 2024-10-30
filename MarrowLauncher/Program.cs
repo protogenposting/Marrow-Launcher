d00852522647be58c9d5ca8d6ccee6d7686004e4
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-10-29 22:32:27 -0400

    weh

--- MarrowLauncher/Program.cs
+++ MarrowLauncher/Program.cs
@@ -107,10 +107,6 @@ else
             Console.WriteLine("Download java here! https://learn.microsoft.com/en-us/java/openjdk/download");
 
             Console.WriteLine("IF THAT DOESN'T WORK GO HERE TO FIGURE OUT JAVA_HOME https://confluence.atlassian.com/doc/setting-the-java_home-variable-in-windows-8895.html");
-
-            Console.WriteLine("Press Enter To Exit...");
-
-            Console.ReadLine();
         }
     }
 }
